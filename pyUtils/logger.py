fb0fae674498d243d76a5f79a4c64b7f495f2d34
Jiajing Xu <mx3xjj@gmail.com>
2020-02-21 01:09:13 -0800

    Update logger.py

--- pyUtils/logger.py
+++ pyUtils/logger.py
@@ -1,12 +1,36 @@
-import logging
-import sys
+def get_module_logger(LOG_NAME = '', LOG_FILE_INFO  = 'file.log', LOG_FILE_ERROR = 'file.err'):
+  
+    LOG_FORMAT     = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    log           = logging.getLogger(LOG_NAME)
+    log_formatter = logging.Formatter(LOG_FORMAT)
+
+    # comment this to suppress console output
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_formatter)
+    log.addHandler(stream_handler)
+
+    file_handler_info = logging.FileHandler(LOG_FILE_INFO, mode='w')
+    file_handler_info.setFormatter(log_formatter)
+    file_handler_info.setLevel(logging.INFO)
+    log.addHandler(file_handler_info)
+
+    file_handler_error = logging.FileHandler(LOG_FILE_ERROR, mode='w')
+    file_handler_error.setFormatter(log_formatter)
+    file_handler_error.setLevel(logging.ERROR)
+    log.addHandler(file_handler_error)
+
+    log.setLevel(logging.INFO)
+
+    return log
+
+def main():
+
+    my_logger = get_logger()
+
+    my_logger.info('This is an INFO message')
+    my_logger.warning('This is a WARNING message')
+    my_logger.error('This is an ERROR message')
 
 
-def get_module_logger(mod_name):
-  logger = logging.getLogger(mod_name)
-  handler = logging.StreamHandler(stream=sys.stdout)
-  formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
-  logger.setLevel(logging.DEBUG)
-  return logger
+if __name__ == '__main__':
+    main()
